2f5d98bcebc561c0cccdfc92002917bd7545bf16
Alex Judy <alex13.judy@gmail.com>
2019-02-08 14:57:28 -0500

    More outlier handling. Checks for if sentence has letters.

--- markovbot/commands.py
+++ markovbot/commands.py
@@ -85,26 +85,32 @@ async def mock(context, user: str = None):
         targeted_message = logs_by_user[0]
         sentence = mock_string(targeted_message.content)
 
+        if sentence is None:
+            await markovbot.say('No alphabetic letters were found in previous message. '
+                                'Try entering letters next time you weeb')
+
         await markovbot.say(sentence)
 
 
 def mock_string(sentence: str):
-    if sentence is None:
-        return
+    char_count = len(sentence)
 
-    char_count = len(sentence) - sentence.count(' ')
+    # Subtract from char_count if not a letter
+    for i in sentence:
+        if not i.isalpha():
+            char_count -= 1
 
     mock_count = math.ceil(char_count - char_count / 2)
     sentence_mock = list(sentence.lower())
 
     index = 0
     current_mock_count = 0
-    loop_limit = 3
+    loop_limit = 4
     while current_mock_count in range(0, mock_count) and loop_limit > 0:
         capitalize = bool(random.getrandbits(1))
         val = str(sentence_mock[index])
         # Don't want to count a char mutation for spaces / integers / capitals.
-        if capitalize and sentence_mock[index] != ' ' and not (val.isdigit() or val.isupper()):
+        if capitalize and sentence_mock[index] != ' ' and val.isalpha() and not (val.isdigit() or val.isupper()):
             sentence_mock[index] = sentence_mock[index].upper()
             current_mock_count += 1
         index += 1
@@ -113,5 +119,10 @@ def mock_string(sentence: str):
         if index == len(sentence_mock):
             index = 0
             loop_limit -= 1
+
     sentence = ''.join(str(e) for e in sentence_mock)
+
+    if current_mock_count < 1:
+        sentence = 'Last message from user did not contain any letters'
+
     return sentence
